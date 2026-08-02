e439d3cdde3b2c085c59331e8af9abf6bb4c0723
Pascal Malouin <pascal.malouin@gmail.com>
2026-08-02 16:31:15 -0400

    ...

--- src/snake_flask/common/__init__.py
+++ src/snake_flask/common/__init__.py
@@ -1,10 +1,12 @@
-# +-------------------------------------------------------------------- INFO -+
-# | [Snake-Flask/src/snake_flask/common/__init__.py]                          |
-# |                                                                           |
-# | Author      : Pascal Malouin (https://github.com/fantomH)                 |
-# | Created     : 2026-06-30 11:03:18 UTC                                     |
-# | Updated     : 2026-06-30 11:03:18 UTC                                     |
-# | Description : Snake-Common API.                                           |
+# +---------------------------------------------------------------------------+
+# [+] INFO
+# +---------------------------------------------------------------------------+
+# [Snake-Flask/src/snake_flask/common/__init__.py]
+# 
+# Author      : Pascal Malouin (https://github.com/fantomH)
+# Created     : 2026-06-30 11:03:18 UTC
+# Updated     : 2026-07-30 11:03:08 UTC
+# Description : Snake-Flask Common API.
 # +---------------------------------------------------------------------------+
 
 from .extension import SnakeCommon
